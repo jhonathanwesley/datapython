4f95bf984337637b738eb6df896a28053fff5031
Jhonathan Wesley <98562102+jhonathanwesley@users.noreply.github.com>
2024-09-27 22:40:06 -0300

    Lindo 1° projeto - tradingcomdados

---
+++ Projetos/Rastreador de Ações/rastreador.py
@@ -0,0 +1,24 @@
+%pip install yfinance
+import yfinance as yf
+import matplotlib.pyplot as mplt
+
+acoes = ['AAPL', 'MSFT', 'PETR4.SA', 'BBDC4.SA']
+
+dados_acoes = yf.download(acoes,
+                          period = '1d',
+                          start = '2022-01-01')
+
+dados_acoes['Close'].plot()
+mplt.xlabel('Data')
+mplt.ylabel('Preço de Fechamento')
+mplt.title('Rastreador de ações')
+mplt.legend(acoes, fontsize = 8)
+mplt.show()
+
+dados_normalizados = dados_acoes['Close'] / dados_acoes['Close'].iloc[0]
+dados_normalizados.plot()
+mplt.xlabel('Data')
+mplt.ylabel('Preço Normalizado')
+mplt.title('Rastreador de ações')
+mplt.legend(acoes, fontsize = 8)
+mplt.show()
