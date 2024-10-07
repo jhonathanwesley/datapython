1460f02bb52aa71e7ceab588178dc431d322b963
Jhonathan Wesley <98562102+jhonathanwesley@users.noreply.github.com>
2024-10-07 00:53:37 -0300

    Py learn data structures

---
+++ Graduação_Ciência_de_Dados/Disciplina_001 - Introdução a Linguagem Python/Unidade 2 -  Explorando Recursos do Python/teste.py
@@ -0,0 +1,6 @@
+# listcomps
+dev_tools = ['Python', 'Java', 'Node.js', 'Java Script', 'Django', 'Libs', 'VS Code', 'Lua', 'Elixir', 'DBMS']
+print(f'Antes da "listcomp":', dev_tools)
+
+dev_tools = [.lower() for i in dev_tools]
+print(f'\nA lista de ferramentas em letras maiúsculas com uso de "LISTCOMPS":', dev_tools)
